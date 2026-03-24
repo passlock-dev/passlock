52b8e72f2bb847e481533468615fa2c208e4f67a
Toby Hobson <toby.hobson@gmail.com>
2026-03-24 15:54:33 +0000

    Cleanup account page

--- examples/sveltekit/src/routes/account/+page.svelte
+++ examples/sveltekit/src/routes/account/+page.svelte
@@ -102,7 +102,7 @@
 	<title>My Account</title>
 </svelte:head>
 
-<div class="flex h-full w-full flex-col items-center justify-center gap-4 px-4 py-8">
+<div class="flex h-full w-full flex-col items-center justify-start gap-4 px-4 py-8">
 	<fieldset class="mt-4 fieldset max-w-md rounded-lg bg-base-200 p-10 pt-8">
 		<form method="POST" action="?/profile" use:profileEnhance>
 			<h2 class="text-center text-xl font-semibold">My account</h2>
@@ -118,36 +118,40 @@
 				<p class="mt-4 text-center text-sm text-error">{profileSyncError}</p>
 			{/if}
 
-			<label for="givenName" class="label">First name</label>
-			<input
-				id="givenName"
-				type="text"
-				name="givenName"
-				autocomplete="given-name"
-				class={['input w-full', { 'input-error': $profileErrors.givenName }]}
-				bind:value={$profileForm.givenName}
-				required />
-			{#if $profileErrors.givenName}
-				{#each $profileErrors.givenName as error (error)}
-					<span class="text-error">{error}</span>
-				{/each}
-			{/if}
-
-			<label for="familyName" class="label">Last name</label>
-			<input
-				id="familyName"
-				type="text"
-				name="familyName"
-				autocomplete="family-name"
-				class={['input w-full', { 'input-error': $profileErrors.familyName }]}
-				bind:value={$profileForm.familyName}
-				required />
-			{#if $profileErrors.familyName}
-				{#each $profileErrors.familyName as error (error)}
-					<span class="text-error">{error}</span>
-				{/each}
-			{/if}
-
+      <div class="mt-4 flex flex-col gap-2">
+        <div>
+          <label for="givenName" class="label">First name</label>
+          <input
+            id="givenName"
+            type="text"
+            name="givenName"
+            autocomplete="given-name"
+            class={['input w-full mt-2', { 'input-error': $profileErrors.givenName }]}
+            bind:value={$profileForm.givenName}
+            required />
+          {#if $profileErrors.givenName}
+            {#each $profileErrors.givenName as error (error)}
+              <span class="text-error">{error}</span>
+            {/each}
+          {/if}
+        </div>
+        <div>
+          <label for="familyName" class="label">Last name</label>
+          <input
+            id="familyName"
+            type="text"
+            name="familyName"
+            autocomplete="family-name"
+            class={['input w-full mt-2', { 'input-error': $profileErrors.familyName }]}
+            bind:value={$profileForm.familyName}
+            required />
+          {#if $profileErrors.familyName}
+            {#each $profileErrors.familyName as error (error)}
+              <span class="text-error">{error}</span>
+            {/each}
+          {/if}
+        </div>
+      </div>
 			<button class="btn mt-4 btn-block btn-primary">Save name changes</button>
 		</form>
 
@@ -171,31 +175,37 @@
 				<p class="mt-4 text-center text-sm text-error">{emailStatusError}</p>
 			{/if}
 
-			<label for="account-email" class="label">Current email</label>
-			<input
-				id="account-email"
-				type="email"
-				autocomplete="email"
-				class="input w-full text-base-content/60"
-				value={data.email}
-				readonly />
+      <div class="mt-4 flex flex-col gap-2">
+        <div>
+          <label for="account-email" class="label">Current email</label>
+          <input
+            id="account-email"
+            type="email"
+            autocomplete="email"
+            class="input w-full mt-2 text-base-content/60"
+            value={data.email}
+            readonly />
+        </div>
 
-			<label for="new-email" class="label">New email</label>
-			<input
-				id="new-email"
-				type="email"
-				name="email"
-				autocomplete="email"
-				class={['input w-full', { 'input-error': $emailErrors.email }]}
-				bind:value={$emailForm.email}
-				required />
+        <div>
+          <label for="new-email" class="label">New email</label>
+          <input
+            id="new-email"
+            type="email"
+            name="email"
+            autocomplete="email"
+            class={['input w-full mt-2', { 'input-error': $emailErrors.email }]}
+            bind:value={$emailForm.email}
+            required />
 
-			{#if $emailErrors.email}
-				{#each $emailErrors.email as error (error)}
-					<span class="text-error">{error}</span>
-				{/each}
-			{/if}
-
+          {#if $emailErrors.email}
+            {#each $emailErrors.email as error (error)}
+              <span class="text-error">{error}</span>
+            {/each}
+          {/if}
+        </div>
+      </div>
+      
 			<button class="btn mt-4 btn-block btn-primary">Verify new email</button>
 		</form>
 
